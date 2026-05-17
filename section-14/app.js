0d60b8e32853c4d1cdbb20a02387dd32873e4e3f
RomanPR <prikhodjko.roman@gmail.com>
2026-05-17 15:59:16 +0300

    Assignment 5 Done

--- section-14/app.js
+++ section-14/app.js
@@ -36,15 +36,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  User.findById('6a077a299a58095a4d671186')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
