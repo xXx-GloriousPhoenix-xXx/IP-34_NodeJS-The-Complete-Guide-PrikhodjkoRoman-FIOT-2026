144e217f292852dfb17cded3ccd2ea327d3b5c5b
RomanPR <prikhodjko.roman@gmail.com>
2026-05-17 15:48:12 +0300

    Section 14 Part 1

--- section-14/app.js
+++ section-14/app.js
@@ -3,20 +3,38 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://defaultUser:abcd123%21@prikhodjkoromanip34clus.aftawgg.mongodb.net/?appName=PrikhodjkoRomanIP34Cluster';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById('6a077a299a58095a4d671186')
@@ -29,13 +47,12 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://defaultUser:abcd123%21@prikhodjkoromanip34clus.aftawgg.mongodb.net/?appName=PrikhodjkoRomanIP34Cluster'
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
