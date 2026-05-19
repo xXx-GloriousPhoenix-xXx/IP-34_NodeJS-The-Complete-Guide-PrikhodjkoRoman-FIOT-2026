7f1d3ba9d7f42737e966c9f4c8d20abb0fecfdbd
RomanPR <prikhodjko.roman@gmail.com>
2026-05-19 15:02:05 +0300

    Async fix

--- section-35/deno/app.ts
+++ section-35/deno/app.ts
@@ -3,7 +3,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import todosRoutes from './routes/todos.ts';
 import { connect } from './helpers/db_client.ts';
 
-connect();
+await connect();
 
 const app = new Application();
 
