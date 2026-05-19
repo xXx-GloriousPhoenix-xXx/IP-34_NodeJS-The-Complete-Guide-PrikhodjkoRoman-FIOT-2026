55e16c09e6ff6cf7a31feac50a72a929639dfb6d
RomanPR <prikhodjko.roman@gmail.com>
2026-05-19 13:57:31 +0300

    Section 35 Done

--- section-35/deno/app.ts
+++ section-35/deno/app.ts
@@ -1,6 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 import todosRoutes from './routes/todos.ts';
+import { connect } from './helpers/db_client.ts';
+
+connect();
 
 const app = new Application();
 
@@ -9,7 +12,18 @@ app.use(async (ctx, next) => {
   await next();
 });
 
+app.use(async (ctx, next) => {
+  ctx.response.headers.set('Access-Control-Allow-Origin', '*');
+  ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+  if (ctx.request.method === 'OPTIONS') {
+    ctx.response.status = 204;
+    return;
+  }
+  await next();
+});
+
 app.use(todosRoutes.routes());
 app.use(todosRoutes.allowedMethods());
 
-await app.listen({ port: 3000 });
+await app.listen({ port: 8000 });
--- section-35/deno/routes/todos.ts
+++ section-35/deno/routes/todos.ts
@@ -1,43 +1,44 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
+import { ObjectId } from 'npm:mongodb@6.1.0';
+import { getDb } from '../helpers/db_client.ts';
 
 const router = new Router();
 
 interface Todo {
-  id: string;
+  id?: string;
   text: string;
 }
 
-let todos: Todo[] = [];
-
-router.get('/todos', (ctx) => {
-  ctx.response.body = { todos: todos };
+router.get('/todos', async (ctx) => {
+  const todos = await getDb().collection('todos').find().toArray();
+  const transformedTodos = todos.map((todo) => {
+    return { id: todo._id.toString(), text: todo.text };
+  });
+  ctx.response.body = { todos: transformedTodos };
 });
 
 router.post('/todos', async (ctx) => {
-  const data = await ctx.request.body();
+  const data = await ctx.request.body.json();
   const newTodo: Todo = {
-    id: new Date().toISOString(),
-    text: data.value.text,
+    text: data.text,
   };
-
-  todos.push(newTodo);
-
+  const result = await getDb().collection('todos').insertOne(newTodo);
+  newTodo.id = result.insertedId.toString();
   ctx.response.body = { message: 'Created todo!', todo: newTodo };
 });
 
 router.put('/todos/:todoId', async (ctx) => {
-  const tid = ctx.params.todoId;
-  const data = await ctx.request.body();
-  const todoIndex = todos.findIndex((todo) => {
-    return todo.id === tid;
-  });
-  todos[todoIndex] = { id: todos[todoIndex].id, text: data.value.text };
+  const tid = ctx.params.todoId!;
+  const data = await ctx.request.body.json();
+  await getDb()
+    .collection('todos')
+    .updateOne({ _id: new ObjectId(tid) }, { $set: { text: data.text } });
   ctx.response.body = { message: 'Updated todo' };
 });
 
-router.delete('/todos/:todoId', (ctx) => {
-  const tid = ctx.params.todoId;
-  todos = todos.filter((todo) => todo.id !== tid);
+router.delete('/todos/:todoId', async (ctx) => {
+  const tid = ctx.params.todoId!;
+  await getDb().collection('todos').deleteOne({ _id: new ObjectId(tid) });
   ctx.response.body = { message: 'Deleted todo' };
 });
 
